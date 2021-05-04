4b5665a419004427ab93d92116e026d631dd3c96
Admin <mynguyen8333@gmail.com>
2021-05-04 08:12:10 +0700

    lan1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DataUser dataUser = new DataUser(this,"userdb.sqlite",null,1);
+
+    }
+}
