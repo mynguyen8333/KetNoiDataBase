1328ba721a1d333b2e4aaf163e21770fff966782
Admin <mynguyen8333@gmail.com>
2021-05-04 08:12:27 +0700

    lan1

---
+++ app/src/main/java/com/example/myapplication/DataUser.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataUser extends SQLiteOpenHelper {
+    public DataUser(@Nullable Context context,
+                    @Nullable String name,
+                    @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql =" CREATE TABLE user (" +
+                "id INTEGER PRIMARY KEY," +
+                "name TEXT NOT NULL)";
+    }
+    public void addUser(User user){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values=  new ContentValues();
+        values.put("id",user.getId());
+        values.put("name",user.getName());
+        db.insert("user",null,values);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
